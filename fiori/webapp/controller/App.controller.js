5d0b8529c982dea4fad674fac9be2fd557512bfa
cmedranogmz <cmedranogmz@gmail.com>
2024-08-02 22:03:40 +0000

    SAPUI5_Panning_Calentar_2024

---
+++ fiori/webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("cmg.fiori.controller.App", {
+        onInit: function() {
+        }
+      });
+    }
+  );
+  
---
+++ fiori/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["cmg/fiori/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
